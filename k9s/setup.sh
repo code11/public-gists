e025b7aa2b1e347836ab9119ec0e3eba3fbe8127
Octavian David <octavian.david@code11.com>
2025-05-22 16:13:05 +0300

    k9s setup

---
+++ k9s/setup.sh
@@ -0,0 +1,45 @@
+#! /usr/bin/env bash
+
+CONFIG_DIR="$HOME/.config/k9s"
+PLUGINS_DIR="$CONFIG_DIR/plugins"
+SOURCE=https://raw.githubusercontent.com/derailed/k9s/refs/heads/master/plugins
+
+# hide k9s logo
+sed -i"" "s/logoless: false/logoless: true/g" "$CONFIG_DIR/config.yaml"
+
+mkdir -p "$PLUGINS_DIR"
+
+PLUGINS=(
+    debug-container
+    dive
+    external-secrets
+    flux
+    job-suspend
+    remove-finalizers
+    watch-events
+)
+
+printf '%s\n' "${PLUGINS[@]}" |
+    while read -r PLUGIN; do
+        wget -O - "$SOURCE/$PLUGIN.yaml" >"$PLUGINS_DIR/$PLUGIN.yaml"
+    done
+
+CUSTOM_PLUGINS=(
+    https://raw.githubusercontent.com/code11/public-gists/refs/heads/main/k9s/plugins/flux-gitrepo.yaml
+)
+printf '%s\n' "${CUSTOM_PLUGINS[@]}" |
+    while read -r FILE; do
+        wget -O - "$FILE" >"$PLUGINS_DIR/${FILE##*/}"
+    done
+
+# Patch ridiculously long names that clutter K9S interface
+# Flux
+yq eval '.plugins.toggle-helmrelease.description = "Toggle HR"' -i "$PLUGINS_DIR/flux.yaml"
+yq eval '.plugins.toggle-kustomization.description = "Toggle KS"' -i "$PLUGINS_DIR/flux.yaml"
+yq eval '.plugins.get-suspended-helmreleases.description = "Suspended HR"' -i "$PLUGINS_DIR/flux.yaml"
+yq eval '.plugins.get-suspended-kustomizations.description = "Suspended KS"' -i "$PLUGINS_DIR/flux.yaml"
+# Remove finalizers
+yq eval '.plugins.remove_finalizers.description = "Removes finalizers"' -i "$PLUGINS_DIR/remove-finalizers.yaml"
+
+# Add aliases
+yq -i '.aliases.git = "gitrepo"' "$CONFIG_DIR/aliases.yaml"
